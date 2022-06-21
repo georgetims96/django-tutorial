b502a93ac3b50ca3adbe528dbcb3fd9f033030ee
George Tims <george.tims@hotmail.com>
2022-06-21 19:47:15 -0400

    First from Lenovo

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 
 from .models import Question, Choice
 
+# Test comment to see if Git credentials have stored
 class IndexView(generic.ListView):
   template_name = 'polls/index.html'
   context_object_name = 'latest_question_list'
