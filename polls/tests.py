d3b74b878e2e4fa8a2275f8bbbeb0a6199146b2b
George Tims <george.tims@hotmail.com>
2022-06-22 23:18:06 -0400

    Added some tests for QuestionIndexView

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,29 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
+
+# Helper method
+def create_question(question_text, days):
+  time = timezone.now() + datetime.timedelta(days=days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+  def test_no_questions(self):
+    response = self.client.get(reverse('polls:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No polls are available.")
+
+  def test_past_question(self):
+    question = create_question(question_text="Past question.", days=-30)
+    response = self.client.get(reverse('polls:index'))
+    self.assertQuerysetEqual(
+      response.context['latest_question_list'],
+      [question],
+    )
+
 # Create your tests here.
 class QuestionModelTests(TestCase):
     def test_was_published_too_long_ago(self):
